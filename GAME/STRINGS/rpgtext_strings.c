0e31b1d544be75063a6dcd8383b36b375f0afb6a
Fonaldozero <69407113+Fonaldozero@users.noreply.github.com>
2021-07-05 16:26:38 -0300

    Update rpgtext_strings.c

--- GAME/STRINGS/rpgtext_strings.c
+++ GAME/STRINGS/rpgtext_strings.c
@@ -15,8 +15,8 @@ char* remove_spaces(char* string)//Returns a malloced pointer with the minimum s
         x++;//If current and next char is a "space", we jump to the next char
     }
     character_name[y] = '\0'; //Terminates the string with atleast one space AKA ' ';
-    x = strlen(character_name)-1; //String lenght
-    while(character_name[x] == ' '){character_name[x] = '\0'; x--;} //Erase spaces at the end, and the last char becomes a "\0" char
+    x = strlen(character_name)-1; //"strlen(character_name) -1" Because  "strlen(character_name)" would return the '\0' Index on the string
+    while(character_name[x] == ' '){character_name[x] = '\0'; x--;} //Erase spaces at the end, and the last char becomes a '\0' char
 
     return strdup(character_name); //Returns a malloced pointer with the minimum space needed to "save" the string; 
 }
